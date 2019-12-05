afa70d245933b15e03a3c9d50d74bc1c49617f90
Millerqwe <millerqwe@gmail.com>
2019-12-05 12:33:53 +0300

    Create deleting React components

--- react_book (Banks, Porcello)/main_app/src/App.js
+++ react_book (Banks, Porcello)/main_app/src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { unmountComponentAtNode } from 'react-dom';
 import './App.css';
 
-import MemberList from './memberList'
 
-class App extends React.Component {
+const App = () => 
+    <div className="app">
 
-  render() {
-
-    const target = document.getElementById('delete');
-    
-    return (
-      <div id="delete" className="App">
-        <MemberList count={3}/>
-        <button onClick={() => unmountComponentAtNode(target)}>Удалить компонент</button>
-      </div>
-    );
-  }
-}
+    </div>
 
 export default App;
