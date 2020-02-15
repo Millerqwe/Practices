747589a2989f110d0c4102dbb3f1e2773b0d8150
Millerqwe <millerqwe@gmail.com>
2020-02-15 15:51:46 +0300

    Create Timer by Flux

---
+++ react_book (Banks, Porcello)/main_app/src/__components/Flux/countdownDispatcher/index.js
@@ -0,0 +1,14 @@
+import { Dispatcher } from 'flux';
+
+
+class CountdownDispatcher extends Dispatcher {
+    handleAction(action) {
+        console.log('dispatching action:', action);
+        this.dispatch({
+            source: 'VIEW_ACTION',
+            action
+        })
+    }
+}
+
+export default CountdownDispatcher;
--- react_book (Banks, Porcello)/main_app/src/index.js
+++ react_book (Banks, Porcello)/main_app/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Countdown  from './__components/Flux/Countdown';
+import CountdownStore from './__components/Flux/countdownStore'
+import countdownActions from './__components/Flux/countdownActions';
+import CountdownDispatcher from './__components/Flux/countdownDispatcher'
+
+
+const appDispatcher = new CountdownDispatcher();
+const actions = countdownActions(appDispatcher);
+const store = new CountdownStore(10, appDispatcher);
+
+const render = count => ReactDOM.render(
+    <Countdown count={count} {...actions}/>,
+    document.getElementById('root')
+);
+
+store.on('TICK', () => render(store.count));
+store.on('RESET', () => render(store.count));
+render(store.count);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
